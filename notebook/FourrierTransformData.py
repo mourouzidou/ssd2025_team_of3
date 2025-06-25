f88caa93928571a583a8298fdea814152dddae54
Carolin Lindow <lindow@math.uni-heidelberg.de>
2025-06-25 13:50:22 +0200

    added a class that contains methods to fourrier transform data

---
+++ notebook/FourrierTransformData.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class FourrierTranformData:
+    def __init__(self, data):
+        self.data = data
+        self.fft_data = None
+        
+
+    def do_DFT(self ):
+        self.data_fft = np.fft.rfft(self.data)
+        self.frequencies = np.fft.rfftfreq(len(self.data), d=1.0)
+    
+    
