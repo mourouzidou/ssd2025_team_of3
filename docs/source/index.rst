00fd93b6e8d7c592dc507482652208f7af0c793b
Eleni Mourouzidou <elenamuruzidu@gmail.com>
2025-06-26 09:58:03 +0200

    Add documentation

---
+++ docs/source/index.rst
@@ -0,0 +1,25 @@
+.. ssd_course_team3 documentation master file, created by
+   sphinx-quickstart on Thu Jun 26 09:23:48 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+ssd_course_team3 documentation
+==============================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+
+
+
+
+
+
+
--- src/process_data.py
+++ src/process_data.py
@@ -3,8 +3,15 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # create a class ProcessData to load and process data
 class ProcessDataPd:
+    """
+    A class to load and process data from a CSV file.
+    This class provides methods to load data, discard columns by index,
+    filter columns by variance, create the upper triangle of the correlation matrix,
+    and get sorted correlations from the correlation matrix.
+    """
     def __init__(self, file_path: str):
         self.file_path = file_path
         self.data = None
@@ -44,7 +51,27 @@ class ProcessDataPd:
             upper_corr = corr.where(mask)
             return upper_corr
         return None
-
-
-
     
+
+
+    def get_sorted_correlations(correlation_matrix, save_to_txt=False):
+        """
+        Get sorted correlations from the correlation matrix.
+        
+        Parameters:
+        correlation_matrix (DataFrame): The correlation matrix.
+        
+        Returns:
+        Series: Sorted correlations with absolute values.
+        
+
+        """
+        upper_triangular = correlation_matrix.where(np.triu(np.ones(correlation_matrix.shape), k=1).astype(bool))
+        
+        # Flatten and sort by absolute values
+        sorted_correlations = upper_triangular.stack().abs().sort_values(ascending=False)
+        
+        return sorted_correlations
+
+
+
